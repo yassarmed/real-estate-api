34e3cab0446a993487b8cd2c66f24b0eb26b5183
Yassar Ahmed <yassarahmed17@gmail.com>
2023-07-17 19:19:17 -0500

    adds create action

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -8,4 +8,19 @@ class HomesController < ApplicationController
     @home = Home.find_by(id: params[:id])
     render :show
   end
+
+  def create
+    @home = Home.create(
+      description: params[:description],
+      year_built: params[:year_built],
+      square_feet: params[:square_feet],
+      bedrooms: params[:bedrooms],
+      bathrooms: params[:bathrooms],
+      floors: params[:floors],
+      availability: params[:availability],
+      address: params[:address],
+      price: params[:price],
+
+    )
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   get "/homes" => "homes#index"
   get "/homes/:id" => "homes#show"
+  post "/homes" => "homes#create"
 end
